54579379c922a4b2163b92c5c7489f423b11d068
Saleh Hindi <shindi@haverford.edu>
2016-09-18 02:32:32 -0400

    State 1 workflow

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.post('/webhook/', function (req, res) {
         	}
             let text = event.message.text
             if (event.postback) {
-				let text = JSON.stringify(event.postback)
+				let text = JSON.stringify(event.postback.payload)
 		        sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
 		        continue
             }
